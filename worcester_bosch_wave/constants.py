4c0e97c36f713da8a1ab929ed80211cde1de7152
Juan Garcia Alvite <juanjsebgarcia@users.noreply.github.com>
2021-12-06 22:22:27 +0000

    Add fixed constants

--- worcester_bosch_wave/constants.py
+++ worcester_bosch_wave/constants.py
@@ -3,3 +3,10 @@ SECRET = b'X\xf1\x8dp\xf6g\xc9\xc7\x9e\xf7\xdeC[\xf0\xf9\xb1U;\xbbna\x81b\x12\xa
 
 # Command path base URI
 PATH_BASE = '/heatingCircuits/hc1/'
+
+# API Constants
+ON = 'on'
+OFF = 'off'
+
+MANUAL = 'manual'
+CLOCK = 'clock'
--- worcester_bosch_wave/wave_thermo.py
+++ worcester_bosch_wave/wave_thermo.py
@@ -1,7 +1,13 @@
 from set import WaveSet
 from status import WaveStatus
 
-from worcester_bosch_wave.constants import PATH_BASE
+from worcester_bosch_wave.constants import (
+    CLOCK,
+    MANUAL,
+    OFF,
+    ON,
+    PATH_BASE,
+)
 
 
 class WaveThermostat:
@@ -14,27 +20,32 @@ class WaveThermostat:
             serial_number=serial_number, access_code=access_code, password=password
         )
 
-    def set_mode(self, mode):
+    def set_base_mode(self, mode):
         """
-        Set the control mode of the thermostat
+        Set the base control mode of the thermostat (when not overridden)
 
-        mode: Control mode, either "manual" or "clock"
+        mode: Control mode, either MANUAL or CLOCK
         """
         self.setter.post_message(f'{PATH_BASE}usermode', mode)
 
     def set_temperature(self, temperature):
+        """
+        Set the temperature to the float provided
+
+        :param temperature: Desired temperature in degrees celsius, 0.5c increments
+        """
         self.status.update()
 
-        if self.status.program_mode == 'manual':
+        if self.status.program_mode == MANUAL:
             self.setter.post_message(f'{PATH_BASE}temperatureRoomManual', temperature)
         else:
             self.setter.post_message(
                 f'{PATH_BASE}manualTempOverride/temperature', temperature
             )
-            self.setter.post_message(f'{PATH_BASE}manualTempOverride/status', 'on')
+            self.override(should_override=True)
 
     def override(self, should_override):
         if should_override:
-            self.setter.post_message(f'{PATH_BASE}manualTempOverride/status', 'on')
+            self.setter.post_message(f'{PATH_BASE}manualTempOverride/status', ON)
         else:
-            self.setter.post_message(f'{PATH_BASE}manualTempOverride/status', 'off')
+            self.setter.post_message(f'{PATH_BASE}manualTempOverride/status', OFF)
